899de9221f310d3fc5d7e48dc7e0eda970df28a4
Praz314159 <praz.ramakrishna@gmail.com>
2020-10-12 17:13:08 -0400

    added more reference papers for literature review

--- build_coauthor_network.py
+++ build_coauthor_network.py
@@ -34,7 +34,7 @@ author_dict = {}
 
 #again, inefficient, but will suffice for now --> structured for readability
 #traversing list of coauthor_groups for each paper 
-i = 0 
+#i = 0 
 for coauthor_group in coauthor_groups:
     #for each coauthor group (1 coauthor goru <--> 1 paper), convert group to list of names   
     coauthor_names_unclean = coauthor_group[0].split(';')
@@ -45,7 +45,7 @@ for coauthor_group in coauthor_groups:
         #adding each scrupped coauthor name to the coauthor_group_members list 
         coauthor_group_members.append(coauthor.strip())
     
-    print("\n\nGROUP MEMBERS: ", coauthor_group_members)
+    #print("\n\nGROUP MEMBERS: ", coauthor_group_members)
  
     coauthor_dict = {}
     for author in coauthor_group_members:
@@ -54,13 +54,13 @@ for coauthor_group in coauthor_groups:
     for author in coauthor_group_members: 
         author_dict.update({author: coauthor_dict})
     
-    print(author_dict)
-    i += 1 
-    if i == 10: 
-        break
+    #print(author_dict)
+    #i += 1 
+    #if i == 25: 
+    #    break
 
 coauthor_network = nx.from_dict_of_dicts(author_dict)
-nx.draw(coauthor_network, with_labels = True)
+nx.draw(coauthor_network, node_size = 50)
 plt.show() 
 #Note that we still have to fix semicolon delimiters
 #motherfucker, can only build nx graphs from dictionary of dictionaries 
